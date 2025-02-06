0125dc193f1d20faa67d719740897843eb3c0b1d
artonb <artonb@live.se>
2025-02-06 15:12:14 +0100

    Clean commit with only latest changes

---
+++ Program.cs
@@ -0,0 +1,122 @@
+using Azure.Identity;
+using DataTrust.Data;
+using DataTrust.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var keyvaultUri = new Uri("https://molntjanster.vault.azure.net/");
+
+builder.Configuration.AddAzureKeyVault(keyvaultUri, new DefaultAzureCredential());
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "Google";
+})
+.AddCookie(options =>
+{
+    // When a user logs in to Google for the first time, create a local account for that user in our database.
+    options.Events.OnValidatePrincipal += async context =>
+    {
+        var serviceProvider = context.HttpContext.RequestServices;
+        using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+
+        string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
+        string name = context.Principal.FindFirst(ClaimTypes.Name).Value;
+
+        var account = db.Accounts
+            .FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
+
+        if (account == null)
+        {
+            account = new Account
+            {
+                OpenIDIssuer = issuer,
+                OpenIDSubject = subject,
+                Name = name
+            };
+            db.Accounts.Add(account);
+        }
+        else
+        {
+            // If the account already exists, just update the name in case it has changed.
+            account.Name = name;
+        }
+
+        await db.SaveChangesAsync();
+    };
+})
+.AddOpenIdConnect("Google", options =>
+{
+    options.Authority = "https://accounts.google.com";
+    options.ClientId = builder.Configuration["Authentication-Google-ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication-Google-ClientSecret"];
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.CallbackPath = "/signin-oidc-google";
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+    options.SaveTokens = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
+
+    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AccessControl>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    SampleData.Create(context);
+}
+
+app.Run();
